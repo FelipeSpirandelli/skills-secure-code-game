7e264d628c28d5ee3e968d7a6cf621a46d916fb0
Felipe Spirandelli <felipereisspirandelli@gmail.com>
2024-06-11 10:49:12 -0300

    feat: solve Level-2

--- Season-1/Level-2/tests.c
+++ Season-1/Level-2/tests.c
@@ -7,14 +7,15 @@
 
 #include "code.h"
 
-int main() {
+int main()
+{
     printf("Level 2 \n\n");
     // Creates a non-admin username called "pwned"
     int user1 = create_user_account(false, "pwned");
     printf("0. Non-admin (admin:%i) username called '%s' has been created \n\n", is_admin(user1), username(user1));
 
     printf("1. Non-admin users like '%s' can update some dummy numerical settings \n", username(user1));
-    printf("2. Non-admin users have no access to settings that can escalate themselves to admins \n\n"); 
+    printf("2. Non-admin users have no access to settings that can escalate themselves to admins \n\n");
 
     // Updates the setting '1' of the pwned username to the number '10'
     update_setting(user1, "1", "10");
@@ -23,6 +24,6 @@ int main() {
 
     if (!is_admin(user1))
         printf("User is not an admin so the code works as expected... is it though? \n");
-        
+
     return 0;
 }
